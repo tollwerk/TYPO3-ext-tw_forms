bcc1fe8f777774f1d81e913469a2a67df4ce95e0
Klaus Fiedler <klaus@tollwerk.de>
2025-12-19 12:36:19 +0100

    Remove wrongly copied code
    Changelog: fixed

--- Configuration/TCA/Overrides/tx_powermail_domain_model_field.php
+++ Configuration/TCA/Overrides/tx_powermail_domain_model_field.php
@@ -1,19 +1,5 @@
 <?php
 defined('TYPO3') || die();
 
-call_user_func(function(){
-    $extensionKey = 'tw_forms';
+$GLOBALS['TCA']['tx_powermail_domain_model_field']['columns']['text']['config']['enableRichtext'] = true;
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $extensionKey,
-        'Configuration/TypoScript',
-        'Tollwerk Enhanced TYPO3 forms'
-    );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $extensionKey,
-        'Configuration/TypoScript/Styles',
-        'Tollwerk Enhanced TYPO3 forms: Styles',
-    );
-
-    $GLOBALS['TCA']['tx_powermail_domain_model_field']['columns']['text']['config']['enableRichtext'] = true;
-});
