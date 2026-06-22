5ea2ac1eb1e4342f4f807e2e0b91f832feaed8c8
Klaus Fiedler <klaus@tollwerk.de>
2026-06-22 14:04:54 +0200

    Fix TCA error when powermail is not installed

--- Configuration/TCA/Overrides/tx_powermail_domain_model_field.php
+++ Configuration/TCA/Overrides/tx_powermail_domain_model_field.php
@@ -1,5 +1,7 @@
 <?php
 defined('TYPO3') || die();
 
-$GLOBALS['TCA']['tx_powermail_domain_model_field']['columns']['text']['config']['enableRichtext'] = true;
-
+// Customize Powermail extension.
+if (!empty($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['powermail'])) {
+    $GLOBALS['TCA']['tx_powermail_domain_model_field']['columns']['text']['config']['enableRichtext'] = true;
+}
